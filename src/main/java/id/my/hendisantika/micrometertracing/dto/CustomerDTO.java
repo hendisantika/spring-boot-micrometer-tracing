840774a648cc7c446e734e9b8677001a8bb1ce9e
hendisantika <hendisantika@yahoo.co.id>
2024-04-25 07:03:40 +0700

    Add CustomerDTO 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/micrometertracing/dto/CustomerDTO.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.micrometertracing.dto;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-micrometer-tracing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/25/24
+ * Time: 07:03
+ * To change this template use File | Settings | File Templates.
+ */
+public record CustomerDTO(String name) {
+}
