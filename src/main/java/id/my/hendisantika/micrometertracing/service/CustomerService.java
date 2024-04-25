5d983da1b4396e2afefde3a4923e87bfa0781757
hendisantika <hendisantika@yahoo.co.id>
2024-04-25 07:05:11 +0700

    Add CustomerService 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/micrometertracing/service/CustomerService.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.micrometertracing.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-micrometer-tracing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/25/24
+ * Time: 07:04
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@Slf4j
+public class CustomerService {
+}
