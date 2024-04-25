bde6b5d47977bef85b5f875e6e80bbc8c2e39c1f
hendisantika <hendisantika@yahoo.co.id>
2024-04-25 07:06:06 +0700

    Add CustomerController 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/micrometertracing/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.micrometertracing.controller;
+
+import id.my.hendisantika.micrometertracing.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-micrometer-tracing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/25/24
+ * Time: 07:05
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+}
