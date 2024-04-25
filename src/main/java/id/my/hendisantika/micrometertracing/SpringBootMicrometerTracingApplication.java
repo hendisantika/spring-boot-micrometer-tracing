c9779bbf244583d5bd7fe4be61a45b971f041533
hendisantika <hendisantika@yahoo.co.id>
2024-04-25 07:01:55 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/micrometertracing/SpringBootMicrometerTracingApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.micrometertracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicrometerTracingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMicrometerTracingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/micrometertracing/SpringBootMicrometerTracingApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.micrometertracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicrometerTracingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
